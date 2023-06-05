3647923b3fa7d914afc2ca6abb174b52cc7f555d
Abass Adeyemi <abassadeyemi1@gmail.com>
2023-06-05 17:34:16 +0100

    Rearranged modules

--- order-service/order-domain/order-domain-core/src/main/java/com/hayba/order/service/domain/valueobject/OrderItemId.java
+++ order-service/order-domain/order-domain-core/src/main/java/com/hayba/order/service/domain/valueobject/OrderItemId.java
@@ -1,9 +1,6 @@
 package com.hayba.order.service.domain.valueobject;
 
 import com.hayba.domain.valueobject.BaseId;
-import com.hayba.domain.valueobject.OrderId;
-
-import java.util.UUID;
 
 public class OrderItemId extends BaseId<Long> {
     public OrderItemId(Long value) {
